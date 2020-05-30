46264b717590cd4975817d18fc91ebfd62c93b0e
aimenin <artem.igorevich8@rambler.ru>
2020-05-30 21:28:58 +0300

    Initial commit

---
+++ build/MatchResult.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// enum - enumeration - объект, который хранит близкие по смыслу данные
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult = exports.MatchResult || (exports.MatchResult = {}));
+;
---
+++ src/MatchResult.ts
@@ -0,0 +1,6 @@
+// enum - enumeration - объект, который хранит близкие по смыслу данные
+export enum MatchResult { // это не только класс, но и новый тип данных
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = 'D'
+};
